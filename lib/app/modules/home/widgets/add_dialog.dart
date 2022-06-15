e33c40c772f6a2a88c6262623bd8f276bdd0e4fe
carol <carol@LAPTOP-IJ353F7M>
2022-06-15 17:51:03 +0400

    TextFormField validate user input

--- lib/app/modules/home/widgets/add_dialog.dart
+++ lib/app/modules/home/widgets/add_dialog.dart
@@ -14,31 +14,73 @@ class AddDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(3.0.wp),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                IconButton(onPressed: () {
-                  Get.back();
-                },
-                 icon: const Icon(Icons.close),
-                 ),
-                 TextButton(
-                  style: ButtonStyle(
-                    overlayColor: MaterialStateProperty.all(Colors.transparent)
-                  ),
-                  onPressed: () {},
-                  child: Text('Terminer',
-                  style: TextStyle(fontSize: 14.0.sp,),
+      body: Form(
+        key: homeCtrl.formKey,
+        child: ListView(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(3.0.wp),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(onPressed: () {
+                    Get.back();
+                  },
+                   icon: const Icon(Icons.close),
+                   ),
+                   TextButton(
+                    style: ButtonStyle(
+                      overlayColor: MaterialStateProperty.all(Colors.transparent)
+                    ),
+                    onPressed: () {},
+                    child: Text('Terminer',
+                    style: TextStyle(fontSize: 14.0.sp,),
+                    )
                   )
-                )
-              ],
+                ],
+              ),
             ),
-          )
-        ],
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 5.0.wp),
+              child: Text(
+                'Nouvelle tâche',
+                style: TextStyle(
+                  fontSize: 20.0.sp,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 5.0.wp),
+              child: TextFormField(
+                controller: homeCtrl.editCtrl,
+                decoration: InputDecoration(
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey[400]!),
+                  )
+
+                ),
+                autofocus: true,
+                validator: (value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return 'Veuillez entrer votre todo item';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 5.0.wp, left: 5.0.wp, right: 5.0.wp, bottom: 5.0.wp),
+              child: Text(
+                'Ajouter à...',
+                style: TextStyle(
+                  fontSize: 14.0.sp,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+          ],
+        ),
       )
     );
   }
