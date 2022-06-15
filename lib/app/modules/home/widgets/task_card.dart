47db1c0e8bf5c754ec7b8b77a271a5169251bfb0
carol <carol@LAPTOP-IJ353F7M>
2022-06-15 13:37:05 +0400

    Gradient progress indicator & Style Task Card

--- lib/app/modules/home/widgets/task_card.dart
+++ lib/app/modules/home/widgets/task_card.dart
@@ -4,6 +4,7 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
+import 'package:step_progress_indicator/step_progress_indicator.dart';
 import 'package:task_todo_app/app/core/utils/extensions.dart';
 import 'package:task_todo_app/app/modules/home/controller.dart';
 
@@ -35,7 +36,52 @@ class TaskCard extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          
+          StepProgressIndicator(
+          //TODO change après la fin du CRUD
+            totalSteps: 100,
+            currentStep: 80,
+            size: 5,
+            padding: 0,
+            selectedGradientColor: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [color.withOpacity(0.5), color],
+            ),
+            unselectedGradientColor: const LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Colors.white, Colors.white],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(6.0.wp),
+            child: Icon(
+              IconData(task.icon, fontFamily: 'MaterialIcons'),
+              color: color,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(6.0.wp),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  task.title,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 12.0.sp,
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                ),
+                SizedBox(height: 2.0.wp),
+                Text('${task.todos?.length ?? 0} Tâche',
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey,
+                ),),
+              ],
+            ),
+          )
         ],
       ),
     );
