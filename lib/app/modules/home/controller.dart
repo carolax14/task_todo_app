91c839b0209bde04d02c479fff0676ff0c17b070
carol <carol@LAPTOP-IJ353F7M>
2022-06-27 10:47:29 +0400

    Get total task & total done task

--- lib/app/modules/home/controller.dart
+++ lib/app/modules/home/controller.dart
@@ -151,5 +151,28 @@ class HomeController extends GetxController {
     return res;
   }
 
+  int getTotalTask() {
+    var res = 0;
+    for (int i = 0; i < tasks.length; i++) {
+      if(tasks[i].todos != null) {
+        res += tasks[i].todos!.length;
+      }
+    }
+    return res;
+  }
+
+  int getTotalDoneTask() {
+    var res = 0;
+    for (int i = 0; i < tasks.length; i++) {
+      if(tasks[i].todos != null) {
+        for (int j = 0; j < tasks[i].todos!.length; j++) {
+          if (tasks[i].todos![j]['done'] == true) {
+            res += 1;
+          }
+        }
+      }
+    }
+    return res;
+  }
   
 }
