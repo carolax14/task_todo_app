aeeb913bf444a0fa910026c6c6ad881a09deffbb
carol <carol@LAPTOP-IJ353F7M>
2022-06-27 10:58:52 +0400

    Layout report page

--- lib/app/modules/report/view.dart
+++ lib/app/modules/report/view.dart
@@ -1,15 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:task_todo_app/app/core/utils/extensions.dart';
+import 'package:task_todo_app/app/modules/home/binding.dart';
+import 'package:task_todo_app/app/modules/home/controller.dart';
 
 class ReportPage extends StatelessWidget {
-  const ReportPage({Key? key}) : super(key: key);
+  final homeCtrl = Get.find<HomeController>();
+  ReportPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Report Page'),
-      ),
+      body: SafeArea(
+        child: Obx(() {
+          var createdTasks = homeCtrl.getTotalTask();
+          var completedTasks = homeCtrl.getTotalDoneTask();
+          var livetasks = createdTasks - completedTasks;
+          var precent = (completedTasks / createdTasks * 100).toStringAsFixed(0);
+          return ListView(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(4.0.wp),
+                child: Text('Mon Rapport',
+            style: TextStyle(
+                fontSize: 24.0.sp,
+                fontWeight: FontWeight.bold
+                ),),
+              ),
+            ],
+          );
+        }),
+      )
     );
   }
 }
